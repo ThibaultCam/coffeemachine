a88d1a341ba692996e4f53780c0f32c80a4debbc
Thibault Cam <cam@intechinfo.fr>
2019-06-24 23:09:45 +0200

    Add count prod

--- CoffeeMachine/CoffeeMachine/DrinkFactory.cs
+++ CoffeeMachine/CoffeeMachine/DrinkFactory.cs
@@ -8,22 +8,44 @@ namespace CoffeeMachine
 {
     public static class DrinkFactory
     {
+        static string[] numbers = new string[] { "no", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+        static Report report = new Report();
+
         public static string CreateDrink(string order)
         {
             string[] ingredients = order.Split(':');
-            StringBuilder stringBuilder = new StringBuilder("Drink maker makes ");
+            StringBuilder stringBuilder = new StringBuilder("Drink maker ");
 
             // Adding drink
             switch (ingredients[0])
             {
                 case "C":
-                    stringBuilder.Append("1 coffee ");
+                    stringBuilder.Append("makes 1 coffee ");
+                    report.NbCoffee++;
                     break;
+                case "Ch":
+                    stringBuilder.Append("will make an extra hot coffee ");
+                    report.NbHotCoffee++;
+                    break;
+                case "O":
+                    report.NbOrangeJuice++;
+                    return stringBuilder.Append("will make one orange juice").ToString();
                 case "T":
-                    stringBuilder.Append("1 tea ");
+                    report.NbTea++;
+                    stringBuilder.Append("makes 1 tea ");
                     break;
                 case "H":
-                    stringBuilder.Append("1 chocolate ");
+                    report.NbChocolate++;
+                    stringBuilder.Append("makes 1 chocolate ");
+                    break;
+                case "Hh":
+                    report.NbHotChocolate++;
+                    stringBuilder.Append("will make an extra hot chocolate ");
+                    break;
+                case "Th":
+                    report.NbHotTea++;
+                    stringBuilder.Append("will make an extra hot tea ");
                     break;
                 default:
                     return "Bad order";
@@ -32,16 +54,18 @@ namespace CoffeeMachine
             // Adding sugar
             int nbSugar = 0;
             Int32.TryParse(ingredients[1], out nbSugar);
-            if(nbSugar > 0)
-                stringBuilder.Append("with " + nbSugar +" sugar" + ((nbSugar>1)? "s " : " "));
+            if (ingredients[0].Length > 1)
+                stringBuilder.Append("with " + numbers[nbSugar] + " sugar");
+            else if (nbSugar > 0)
+                stringBuilder.Append("with " + nbSugar +" sugar" + ((nbSugar>1)? "s" : ""));
             else
-                stringBuilder.Append("with no sugar ");
+                stringBuilder.Append("with no sugar");
 
             // Adding stick
             if(ingredients[2] == "0")
-                stringBuilder.Append("and a stick");
-            else
-                stringBuilder.Append("and no stick");
+                stringBuilder.Append(" and a stick");
+            else if(ingredients[0].Length < 2)
+                stringBuilder.Append(" and no stick");
 
             return stringBuilder.ToString();
         }
@@ -51,15 +75,22 @@ namespace CoffeeMachine
             string[] ingredients = order.Split(':');
             switch (ingredients[0])
             {
+                case "O":
+                    if (money < 0.6)
+                        return "You need " + (0.6 - money).ToString();
+                    break;
                 case "C":
+                case "Ch":
                     if (money < 0.6)
                         return "You need " + (0.6 - money).ToString();
                     break;
                 case "T":
+                case "Th":
                     if (money < 0.4)
                         return "You need " + (0.4 - money).ToString();
                     break;
                 case "H":
+                case "Hh":
                     if (money < 0.5)
                         return "You need " + (0.5 - money).ToString();
                     break;
@@ -68,6 +99,16 @@ namespace CoffeeMachine
             }
 
             return CreateDrink(order);
+        }
+
+        public static List<string> Report()
+        {
+            return report.Sold();
+        }
+
+        public static void ResetData()
+        {
+            report = new Report();
         }
     }
 }
---
+++ CoffeeMachine/CoffeeMachine/Report.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMachine
+{
+    public class Report
+    {
+        public int NbTea { get; set; }
+        public int NbHotTea { get; set; }
+        public int NbCoffee { get; set; }
+        public int NbHotCoffee { get; set; }
+        public int NbChocolate { get; set; }
+        public int NbHotChocolate { get; set; }
+        public int NbOrangeJuice { get; set; }
+
+        public List<string> Sold()
+        {
+            List<string> list = new List<string>();
+            list.Add("Number of tea : " + NbTea.ToString());
+            list.Add("Number of hot tea : " + NbHotTea.ToString());
+            list.Add("Number of coffee : " + NbCoffee.ToString());
+            list.Add("Number of hot coffee : " + NbHotCoffee.ToString());
+            list.Add("Number of chocolate : " + NbChocolate.ToString());
+            list.Add("Number of hot chocolate : " + NbHotChocolate.ToString());
+            list.Add("Number of orange juice : " + NbOrangeJuice.ToString());
+
+            return list;
+        }
+    }
+}
